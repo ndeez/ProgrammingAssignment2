d47f1aca4f1cbd2f7246b61f19fc2483a04881d1
ndee <deveauxmarc@gmail.com>
2016-01-23 20:51:12 +0100

    assignement coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
 
+## function to save matrix data in the cache, set() for the matrix, setmatrix() for the inverse value.
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    #set the matrice we are going to use 
+    #ensure that the object m which will store the inverse of y does not contain any value  
+    set <- function(y) {
+        print(y)
+        x <<- y
+        j <<- y 
+        m <<- NULL
+    }
+    #to get the value 
+    get <- function() x
+    #to set the matrice inverse 
+    setmatrix <- function(inverse) m <<- inverse
+    #to get the matrice inverse
+    getmatrix <- function() m
+    list(set = set, get = get,
+         setmatrix = setmatrix,
+         getmatrix = getmatrix)
 }
 
 
-## Write a short comment describing this function
+## Gives the inverse matrix or calculates it if needed
+    # If the cache for the inverse matrice is empty : calculates the inverse matrix with makecachematrix solve(get())
+    # If the cache for the inverse matrice is full, check if the cache is similar to the makecachematrix solve(get())
+    # If it wasn't, it calculates the inverse matrix with makecachematrix get() / If it was, gives the cache value
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x= matrix(), ...) {
+
+    m <- x$getmatrix()
+    if(!is.null(m))  {
+        if(is.matrix(x$get()) && is.matrix(solve(m)) && dim(x$get()) == dim(solve(m)) && all(x$get() == solve(m))) {
+           message("getting cached data")
+            return(m) 
+        }
+    message("the cache inverse matrix and the matrix don't match. The last matrix will have it inverse calculated")
+        
+    }
+    matrix <- x$get()
+    m <- solve(matrix, ...)
+    x$setmatrix(m)
+    m
+    
 }
+
+
+
+
